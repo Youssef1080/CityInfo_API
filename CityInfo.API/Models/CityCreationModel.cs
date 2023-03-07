6313a594d4a4199d1e4efcaaaa3979e033548dc7
Youssef Ahmed <joesadek11@gmail.com>
2023-03-07 14:37:09 +0200

    Added This For Creating city Model

---
+++ CityInfo.API/Models/CityCreationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CityCreationModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int NumberOfPointsOfInterist => PointsOfInteristsList.Count;
+        public List<PointsOfInteristModel> PointsOfInteristsList { get; set; } = new List<PointsOfInteristModel>();
+    }
+}
