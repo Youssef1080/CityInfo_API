8bf7d78fdbba90d469ab23c740272c8450f5d508
Youssef Ahmed <joesadek11@gmail.com>
2023-03-08 13:18:27 +0200

    Added Models to add and update data (city, pointOfInterests).

--- CityInfo.API/Models/PointsOfInterestUpdate.cs
+++ CityInfo.API/Models/PointsOfInterestUpdate.cs
@@ -1,8 +1,14 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     public class PointsOfInterestUpdate
     {
+        [Required(ErrorMessage = "enter a value for name")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
