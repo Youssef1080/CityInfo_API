8ed9e1fb10618512b40819493b5e2f285b1bb8a8
Youssef Ahmed <joesadek11@gmail.com>
2023-03-08 13:19:53 +0200

    Add JsonPatchDocument to update models Partially.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -13,8 +13,8 @@ namespace CityInfo.API
             builder.Services.AddControllers(options =>
             {
                 options.ReturnHttpNotAcceptable = true;
-            })
-                .AddXmlDataContractSerializerFormatters();
+            }).AddNewtonsoftJson()
+              .AddXmlDataContractSerializerFormatters();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
