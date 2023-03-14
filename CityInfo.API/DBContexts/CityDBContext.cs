1f09a82a9b1977a846c25bc88fa3573196b6fbbd
Youssef Ahmed <joesadek11@gmail.com>
2023-03-14 14:02:49 +0200

    Add Entity Framework Core and Entities classes and also migrations

---
+++ CityInfo.API/DBContexts/CityDBContext.cs
@@ -0,0 +1,54 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DBContexts
+{
+    public class CityDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>()
+                .HasData(
+                    new City("New York lbn")
+                    {
+                        Id = 1,
+                        Description = "good lbn in new york",
+                    },
+                    new City("New Capital lbn")
+                    {
+                        Id = 2,
+                        Description = "good lbn in new Capital"
+                    }
+                );
+
+            modelBuilder.Entity<PointOfInterest>()
+                .HasData(
+                    new PointOfInterest("samk")
+                    {
+                        Id = 1,
+                        Description = "good samk in new york",
+                        CityId = 1
+                    },
+                    new PointOfInterest("new barkoof lbn")
+                    {
+                        Id = 2,
+                        Description = "good lbn in new barkoof",
+                        CityId = 1,
+                    }
+                );
+        }
+
+        public CityDBContext(DbContextOptions<CityDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+    }
+}
---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string? Description { get; set; }
+
+        public List<PointOfInterest> PointsOfInteristsList { get; set; } = new List<PointOfInterest>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
