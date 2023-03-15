6efcb493627f6b16a822bfe9419d3566d3881016
Youssef Ahmed <joesadek11@gmail.com>
2023-03-15 11:48:03 +0200

    Add City and Point Profiles for using the Automapper

---
+++ CityInfo.API/Profiles/CityProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CityInfo.API.Entities;
+using CityInfo.API.Models;
+
+namespace CityInfo.API.Profiles
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<City, CityWithoutPointModel>();
+            CreateMap<City, CityModel>();
+            CreateMap<CityCreationModel, City>();
+            CreateMap<CityUpdate, City>();
+            CreateMap<City, CityUpdate>();
+        }
+    }
+}
---
+++ CityInfo.API/Profiles/PointOfInterestsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CityInfo.API.Entities;
+using CityInfo.API.Models;
+
+namespace CityInfo.API.Profiles
+{
+    public class PointOfInterestsProfile : Profile
+    {
+        public PointOfInterestsProfile()
+        {
+            CreateMap<PointOfInterest, PointsOfInteristModel>();
+            CreateMap<PointsOfInterestCreationModel, PointOfInterest>();
+            CreateMap<PointsOfInteristModel, PointOfInterest>();
+            CreateMap<PointsOfInterestUpdate, PointOfInterest>();
+            CreateMap<PointOfInterest, PointsOfInterestUpdate>();
+        }
+    }
+}
