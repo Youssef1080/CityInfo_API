24bcbe1178a5af7c6666e10b404beb11c9891bd6
Youssef Ahmed <joesadek11@gmail.com>
2023-03-15 11:45:42 +0200

    Add asynchrones methods for (get, add, update, delete).

--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -18,6 +18,18 @@ namespace CityInfo.API.Services
             return await context.Cities.OrderBy(c => c.Name).ToListAsync();
         }
 
+        public async Task<IEnumerable<City>> GetCitiesAsync(string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return await GetCitiesAsync();
+            }
+
+            name = name.ToLower().Replace(" ", "");
+
+            return await context.Cities.Where(c => c.Name.ToLower().Replace(" ", "") == name).OrderBy(c => c.Name).ToListAsync();
+        }
+
         public async Task<City?> GetCityAsync(int cityId, bool getPoint)
         {
             if (getPoint)
@@ -28,6 +40,16 @@ namespace CityInfo.API.Services
             return await context.Cities.Where(c => c.Id == cityId).FirstOrDefaultAsync();
         }
 
+        public void AddCity(City city)
+        {
+            context.Cities.Add(city);
+        }
+
+        public void RemoveCity(City city)
+        {
+            context.Cities.Remove(city);
+        }
+
         public async Task<PointOfInterest?> GetPointOfInterestAsync(int cityId, int pointId)
         {
             return await context.PointOfInterests.Where(p => p.CityId == cityId && p.Id == pointId).FirstOrDefaultAsync();
@@ -36,6 +58,28 @@ namespace CityInfo.API.Services
         public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterestAsync(int cityId)
         {
             return await context.PointOfInterests.Where(p => p.CityId == cityId).ToListAsync();
+        }
+
+        public async Task AddPointOfInterestAsync(int cityId, PointOfInterest point)
+        {
+            var city = await GetCityAsync(cityId, false);
+
+            city?.PointsOfInteristsList.Add(point);
+        }
+
+        public void RemovePointOfInterest(PointOfInterest point)
+        {
+            context.PointOfInterests.Remove(point);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await context.SaveChangesAsync() >= 0);
+        }
+
+        public async Task<bool> IsCityExist(int cityId)
+        {
+            return await context.Cities.AnyAsync(c => c.Id == cityId);
         }
     }
 }
