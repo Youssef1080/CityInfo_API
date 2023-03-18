15a4adaa69814de919b5bf0db73e4f42120cc539
Youssef Ahmed <joesadek11@gmail.com>
2023-03-18 10:25:59 +0200

    Add Document xml comments

--- CityInfo.API/Models/CityWithoutPointModel.cs
+++ CityInfo.API/Models/CityWithoutPointModel.cs
@@ -1,9 +1,23 @@
 ﻿namespace CityInfo.API.Models
 {
+    /// <summary>
+    /// city class but without the points of Interests List
+    /// </summary>
     public class CityWithoutPointModel
     {
+        /// <summary>
+        /// identifier of city
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// the name of the city
+        /// </summary>
         public string Name { get; set; } = "";
+
+        /// <summary>
+        /// information about the city
+        /// </summary>
         public string? Description { get; set; }
     }
 }
